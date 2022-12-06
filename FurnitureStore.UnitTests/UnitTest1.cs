16b72dd600cb3c1b2b28cd306b56c2d82f103031
nedrett <nedret88@gmail.com>
2022-12-06 15:27:23 +0200

    Initial Create of UnitTests Project

---
+++ FurnitureStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FurnitureStore.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
